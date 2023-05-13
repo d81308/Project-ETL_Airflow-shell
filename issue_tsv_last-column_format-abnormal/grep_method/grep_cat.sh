cbd6c2f4d25d93d257f4d05e3bd5bdab3a263e13
Neil Wu <114141600+d81308@users.noreply.github.com>
2023-05-13 13:28:18 +0800

    Add files via upload

---
+++ issue_tsv_last-column_format-abnormal/grep_method/grep_cat.sh
@@ -0,0 +1,3 @@
+file="/Users/global_user_file/shell_files/airflow_start/project_sixth-step-dag_tester/issue_tsv_last-column_format-abnormal/finding_root-cause/fixed_width_data.csv"
+
+cat $file | grep -o  '[a-zA-Z0-9,]\+'
---
+++ issue_tsv_last-column_format-abnormal/grep_method/troubleshooting.sh
@@ -0,0 +1,34 @@
+file1="/Users/global_user_file/shell_files/airflow_start/project_sixth-step-dag_tester/issue_tsv_last-column_format-abnormal/finding_root-cause/tsv_data.csv"
+file2="/Users/global_user_file/shell_files/airflow_start/project_sixth-step-dag_tester/issue_tsv_last-column_format-abnormal/finding_root-cause/fixed_width_data.csv"
+
+source /Users/global_user_file/shell_files/airflow_start/project_sixth-step-dag_tester/issue_tsv_last-column_format-abnormal/cat_list.sh $file1 $file2
+
+#source /Users/global_user_file/shell_files/airflow_start/project_sixth-step-dag_tester/issue_tsv_last-column_format-abnormal/new_column_solution.sh $file1 $file2
+
+
+##read each elements
+# extract
+read=$(cat $file1)
+list=($read)
+
+line1="${list[1]}"
+
+# key: print each elements
+#echo $line1 | tr " " "," | sed -e 's/\(.\)/\1\n/g'
+
+
+## char process
+#echo ${line1:0:100}
+
+while read -n 1 char ; do
+	if [ -z "$char" ]
+	then
+		echo "empty"
+	else
+		echo "1"
+	fi
+done <<< "$line1"
+
+## grep regex method
+echo "$line1" | grep -o  '[a-zA-Z0-9]\+' | sed -e 's/\(.\)/\1\n/g'
+
